a0a5cca454947cfa242e7fc9f04fabf7b9932adc
BucurEva87 <bucur.eva87@gmail.com>
2022-10-04 11:02:48 +0300

    Update DragonsView to use the Dragon component

--- src/components/DragonsView/DragonsView.js
+++ src/components/DragonsView/DragonsView.js
@@ -3,7 +3,7 @@ import Dragon from '../Dragon/Dragon';
 // import style from './DragonsView.module.scss';
 
 const DragonsView = () => (
-  <div>DragonsView</div>
+  <Dragon />
 );
 
 export default DragonsView;
